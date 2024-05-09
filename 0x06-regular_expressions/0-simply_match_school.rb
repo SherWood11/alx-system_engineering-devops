29c0b7765cb592cb2e5a247f4bc33c738fa81bb7
Ahmed <sherwedahmed1@gmail.com>
2024-05-09 06:41:08 -0700

    RX

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# A regular expression that is simply matching School
-Puts ARGV[0].scan(/School/).join
+# The regular expression must match School
+puts ARGV[0].scan(/School/).join
